9804ae861b11f6a6c56869a88c57b79b1b6b9051
AllKarr <allen.karro@gmail.com>
2025-05-13 19:57:20 +0200

    Baseline Design, No Function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { BudgetsComponent } from './pages/budgets/budgets.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'budgets', component: BudgetsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  username = '';
+  password = '';
+
+  register() {
+    console.log('Register:', this.username, this.password);
+    // TODO: Call AuthService later
+  }
+}
